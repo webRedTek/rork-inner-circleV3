f043a0cf1010d7ca84edb5a12a474c9b47f50604
JamesE <jedwards3g@gmail.com>
2025-08-20 01:46:47 -0400

    Override Metro serializer to bypass importLocationsPlugin dependency

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,18 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+// Override serializer to avoid importLocationsPlugin dependency
+config.serializer = {
+  ...config.serializer,
+  customSerializer: undefined,
+};
+
+// Remove the problematic reconcileTransformSerializerPlugin
+if (config.serializer && config.serializer.plugins) {
+  config.serializer.plugins = config.serializer.plugins.filter(
+    plugin => !plugin.toString().includes('reconcileTransform')
+  );
+}
+
+module.exports = config;
