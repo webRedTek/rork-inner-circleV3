c5be8dc83b1d067141fe37d0582c1e93ce6744c0
JamesE <jedwards3g@gmail.com>
2025-08-20 01:31:43 -0400

    Simplify Metro config - let Expo handle Metro versions automatically

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,3 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
-
-// Disable importLocationsPlugin that's causing the build to fail
-config.serializer = {
-  ...config.serializer,
-  customSerializer: undefined,
-};
-
-// Remove problematic plugins
-if (config.transformer && config.transformer.plugins) {
-  config.transformer.plugins = config.transformer.plugins.filter(
-    plugin => !plugin.includes('importLocationsPlugin')
-  );
-}
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
