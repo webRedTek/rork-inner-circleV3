d88130c50ce8c061ec19991d3e66e2e94768d3e5
JamesE <jedwards3g@gmail.com>
2025-08-20 02:13:54 -0400

    Try filtering out reconcileTransformSerializerPlugin from Metro config

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,16 @@
+// Minimal Metro config to avoid importLocationsPlugin error
 const { getDefaultConfig } = require("@expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-// Override serializer to remove problematic plugins
-config.serializer = {
-  ...config.serializer,
-  customSerializer: ({ 
-    entryPoint,
-    preModules,
-    graph,
-    options
-  }) => {
-    // Use basic Metro serializer without Expo's problematic plugins
-    const { baseJSBundle } = require("metro/src/DeltaBundler/Serializers/baseJSBundle");
-    return baseJSBundle(entryPoint, preModules, graph, options);
-  }
-};
+// Remove problematic serializer plugins
+config.serializer.customSerializer = undefined;
+
+// Filter out reconcileTransformSerializerPlugin if it exists
+if (config.serializer.plugins) {
+  config.serializer.plugins = config.serializer.plugins.filter(
+    plugin => plugin.name !== 'reconcileTransformSerializerPlugin'
+  );
+}
 
 module.exports = config;
