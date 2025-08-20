7158df5fedf5688f10a44f6c6866e231f7b917ee
JamesE <jedwards3g@gmail.com>
2025-08-20 02:10:18 -0400

    Fix Metro bundler by bypassing problematic importLocationsPlugin

--- metro.config.js
+++ metro.config.js
@@ -1,2 +1,20 @@
 const { getDefaultConfig } = require("@expo/metro-config");
-module.exports = getDefaultConfig(__dirname);
+
+const config = getDefaultConfig(__dirname);
+
+// Override serializer to remove problematic plugins
+config.serializer = {
+  ...config.serializer,
+  customSerializer: ({ 
+    entryPoint,
+    preModules,
+    graph,
+    options
+  }) => {
+    // Use basic Metro serializer without Expo's problematic plugins
+    const { baseJSBundle } = require("metro/src/DeltaBundler/Serializers/baseJSBundle");
+    return baseJSBundle(entryPoint, preModules, graph, options);
+  }
+};
+
+module.exports = config;
