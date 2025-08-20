49df47275b99cb3bfb8dd286936de689f1e598bb
JamesE <jedwards3g@gmail.com>
2025-08-20 02:02:17 -0400

    Upgrade project to Expo SDK 53 + React 18.2 + RN 0.71.8 with aligned package versions

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,2 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Override serializer to avoid importLocationsPlugin dependency
-config.serializer = {
-  ...config.serializer,
-  customSerializer: undefined,
-};
-
-// Remove the problematic reconcileTransformSerializerPlugin
-if (config.serializer && config.serializer.plugins) {
-  config.serializer.plugins = config.serializer.plugins.filter(
-    plugin => !plugin.toString().includes('reconcileTransform')
-  );
-}
-
-module.exports = config;
+const { getDefaultConfig } = require("@expo/metro-config");
+module.exports = getDefaultConfig(__dirname);
